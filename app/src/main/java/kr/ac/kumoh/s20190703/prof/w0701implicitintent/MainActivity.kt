47a95d712fe417abd32a4acd68e25cf69ecded03
엄태욱 <utsi09@naver.com>
2022-10-19 16:37:22 +0900

    edit

--- app/src/main/java/kr/ac/kumoh/s20190703/prof/w0701implicitintent/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20190703/prof/w0701implicitintent/MainActivity.kt
@@ -12,10 +12,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         binding.btnHomepage.setOnClickListener{
             val uri = Uri.parse("https://naver.com")
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
         }
+
+        binding.btnMap.setOnClickListener{
+            val uri = Uri.parse("geo:0.0?z=17&q= 경기도 용인시 영덕동 경남아너스빌 11단지")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        binding.btnTelephone.setOnClickListener{
+            val uri = Uri.parse("sms : 010-2854-2293")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+
     }
 }
